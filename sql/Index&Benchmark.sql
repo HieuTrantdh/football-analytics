0b8bc0bc4eea010ea1d16c2006b0f76c35d31cb4
tranquangduy73 <duytranquang73@gmail.com>
2026-04-12 23:55:13 +0700

    Update Index&Benchmark.sql

--- sql/Index&Benchmark.sql
+++ sql/Index&Benchmark.sql
@@ -1,14 +1,10 @@
-USE football_db;
+USE football_analytics;
 -- Benchmark trước khi thêm index
-ALTER TABLE `match` DROP INDEX idx_matches_match_date;
+ALTER TABLE matches DROP INDEX idx_matches_match_date;
 EXPLAIN FORMAT=TRADITIONAL
-SELECT SQL_NO_CACHE * FROM `match` WHERE match_date = '2015-12-20';
+SELECT SQL_NO_CACHE * FROM matches WHERE match_date = '2015-12-20';
 -- Thêm index
-ALTER TABLE `match` ADD INDEX idx_matches_match_date (match_date);
+ALTER TABLE matches ADD INDEX idx_matches_match_date (match_date);
 -- Benchmark sau khi thêm index
 EXPLAIN FORMAT=TRADITIONAL
-SELECT SQL_NO_CACHE * FROM `match` WHERE match_date = '2015-12-20';
-
-
-
-
+SELECT SQL_NO_CACHE * FROM matches WHERE match_date = '2015-12-20';
