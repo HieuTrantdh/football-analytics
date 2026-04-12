3fc603881af94d3ce97b93712c60d3891f90b5f6
tranquangduy73 <duytranquang73@gmail.com>
2026-04-13 00:09:23 +0700

    Tv4 + Tv5 update index & benchmark

---
+++ sql/Benchmark.sql
@@ -0,0 +1,10 @@
+USE football_analytics;
+-- Benchmark trước khi thêm index
+ALTER TABLE matches DROP INDEX idx_matches_match_date;
+EXPLAIN FORMAT=TRADITIONAL
+SELECT SQL_NO_CACHE * FROM matches WHERE match_date = '2015-12-20';
+-- Thêm index
+ALTER TABLE matches ADD INDEX idx_matches_match_date (match_date);
+-- Benchmark sau khi thêm index
+EXPLAIN FORMAT=TRADITIONAL
+SELECT SQL_NO_CACHE * FROM matches WHERE match_date = '2015-12-20';
